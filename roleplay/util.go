8bb5baa519c811377064c570ccf91720c7b61f19
Araan Branco <araanbranco@protonmail.com>
2020-07-29 00:01:04 -0300

    Add util

---
+++ roleplay/util.go
@@ -0,0 +1,18 @@
+package roleplay
+
+import "fmt"
+
+func LogChestToOwner(ctx Context, message string) {
+	dm, err := ctx.Discord.UserChannelCreate(ctx.Conf.OwnerId)
+	if err != nil {
+		fmt.Println("Erro ao enviar DM: ", err.Error())
+	}
+
+	msg := fmt.Sprintf("Mensagem enviada por: %v \n %v", ctx.Message.Author.Mention(), message)
+
+	// Send message private
+	_, err = ctx.Discord.ChannelMessageSend(dm.ID, msg)
+	if err != nil {
+		fmt.Println("Erro ao enviar DM: ", err)
+	}
+}
